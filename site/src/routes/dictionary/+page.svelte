e60d543d80f65514d91abe205f2cf736dffc2c90
Artemy Egorov <artegoser@gmail.com>
2024-08-30 11:56:44 +0300

    feat: word_types

--- site/src/routes/dictionary/+page.svelte
+++ site/src/routes/dictionary/+page.svelte
@@ -1,6 +1,7 @@
 <svelte:head>
 	<title>Systean dictionary</title>
 </svelte:head>
+
 <div class="flex flex-col items-center">
 	<div class="big-text mt-6">Systean dictionary</div>
 	<div class="small-text">Nothing here now...</div>
